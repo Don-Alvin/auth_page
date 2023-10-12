54e6656b5664e7b42f6d8f94243fd738510cdd70
Don Alvin <alvindon41@gmail.com>
2023-10-12 16:27:54 +0300

    remove extra files

--- src/Features/Auth/hooks/useRefreshToken.jsx
+++
@@ -1,9 +0,0 @@
-import useAuth from "./useAuth"
-
-const useRefreshToken = () => {
-    const { auth } = useAuth()
-    const refreshToken = auth.refresh_token
-    localStorage.setItem('refreshToken', refreshToken)
-}
-
-export default useRefreshToken
